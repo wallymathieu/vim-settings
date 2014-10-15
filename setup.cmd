75b5d55a3f33f4099ba86df296d3a09696e39512
Oskar Gewalli <oskar@gewalli.se>
2014-10-15 07:25:55 +0200

    Looks like this script is working

--- setup.cmd
+++ setup.cmd
@@ -1,12 +1,1 @@
-
-if not exist %HOME%/_vim (
-  mklink %HOME%/vim-settings/vim %HOME%/_vim
-)
-
-if not exist %HOME%/_vimrc (
-  mklink %HOME%/vim-settings/_vimrc %HOME%/_vimrc
-)
-
-if not exist %HOME%/vim-settings/vim/autoload/pathogen.vim ( 
-  mklink %HOME%/vim-settings/vim-pathogen/autoload/pathogen.vim %HOME%/vim-settings/vim/autoload/pathogen.vim
-)
+@powershell -NoProfile -ExecutionPolicy unrestricted -file setup.ps1
---
+++ setup.ps1
@@ -0,0 +1,9 @@
+
+cp -r vim $Home\_vim
+
+cp _vimrc $Home\_vimrc
+
+cp -r . $Home\vim-settings
+
+cp .\vim-pathogen\autoload\pathogen.vim $Home\vim-settings\vim\autoload\pathogen.vim
+
