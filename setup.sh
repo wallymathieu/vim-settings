96d64dc4a869d4ab3da0aeb729511252c0b7773b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-02-23 18:18:24 +0100

    cleanup

--- setup.sh
+++ setup.sh
@@ -1,27 +1,18 @@
 cwd=$(pwd)
 
-if [ ! -e ~/.vim/tmp ]
-then
-  mkdir ~/.vim/tmp
-fi
-
 if [ ! -e ~/.vim ]
 then
   ln -s $cwd/vim ~/.vim
+fi
+
+if [ ! -e ~/.vim/tmp ]
+then
+  mkdir -p ~/.vim/tmp
 fi
 
 if [ ! -e ~/.vimrc ]
 then
   ln -s $cwd/vimrc ~/.vimrc
 fi
-
-cd ~/.vim/bundle/omnisharp/server
-msbuild
-
-cd ~/.vim/bundle/omnisharp/omnisharp-roslyn
-./build.sh
-
-cd $cwd/vim/bundle/vim-fsharp
-make
 
 cd $cwd
